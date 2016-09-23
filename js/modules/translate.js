54bb3c081e1df248caa4c98dc8c93082be0f8b9a
Joey Kamsteeg <joey_kamsteeg@live.nl>
2016-09-23 14:47:51 +0200

    $apply after all languages has been loaded

--- js/modules/translate.js
+++ js/modules/translate.js
@@ -21,6 +21,8 @@ angular.module('translate', ['ngResource'])
                 var language = newConfig.languages[i];
                 this.loadLanguage( language );
             }
+            
+            $rootScope.$apply();
         };
 
         this.loadLanguage = function( code ){
@@ -28,7 +30,6 @@ angular.module('translate', ['ngResource'])
             var language = $resource( url, {}, { query: { method: "GET" } }).query();
 
             this.languages.push( language );
-            $rootScope.$apply();
         };
 
         this.getWord = function( key ){
